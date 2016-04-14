74a83efa5f3680783ed486d87c97174049bac485
maxjack <172617853@qq.com>
2016-04-14 10:58:13 +0800

    首次提交到本地仓库。

---
+++ src/com/xuexi/utilstest/Testqq.java
@@ -0,0 +1,17 @@
+package com.xuexi.utilstest;
+
+public class Testqq {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double[] a= new double[5];
+		a[0]=1;
+		a[1]=2;
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
+
+	
+}
